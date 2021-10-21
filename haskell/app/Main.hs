9154befc0b4c1e925b354de1979ffe8c7e81d9fc
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-10-20 17:50:00 -0700

    Add basic render loop

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -4,6 +4,7 @@ import Lib
 import Tokenizer(tokenize, setupTokenizer)
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
 
 window :: Display
 window = InWindow "programz 4 kidz" (500, 500) (24, 24)
@@ -11,9 +12,15 @@ window = InWindow "programz 4 kidz" (500, 500) (24, 24)
 background :: Color
 background = white
 
-drawing :: Picture
-drawing = circle 80
+data Universe = Empty Float
+
+render :: Universe -> Picture
+render (Empty radius) = circle radius
+
+update :: ViewPort -> Float -> Universe -> Universe
+update _ _ (Empty n) = if n < 80 then Empty (n + 1) else Empty 40
+
 
 main :: IO ()
 main = setupTokenizer >> tokenize "Hello world"
-    >>= print >> display window background drawing
+    >>= print >> simulate window background 60 (Empty 40) render update
