cc38a31eb8c98e7fcf670f231caeeb5ba0753b3a
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-02 19:39:35 -0700

    Simplify code, specify that A* should be used

--- prolog/main.pl
+++ prolog/main.pl
@@ -5,7 +5,6 @@ function("parseInt", ["str"], ["int"], "documentation").
 function("parseInt2", ["str"], ["int"], "documentation").
 function("print", ["int"], ["None"], "documentation").
 function("print2", ["int"], ["None"], "documentation").
-
 function("increment", ["int"], ["int"], "documentation").
 
 name(Func, Name) :- function(Func, _, _, _), Name=Func.
@@ -13,28 +12,23 @@ inputs(Func, Inputs) :- function(Func, Inputs, _, _).
 outputs(Func, Outputs) :- function(Func, _, Outputs, _).
 docs(Func, Documentation) :- function(Func, _, _, Documentation).
 
-listSubset([], B, YesOrNo) :- YesOrNo=yes.
-listSubset([First|Rest], B, YesOrNo) :-
+listSubset([], _).
+listSubset([First|Rest], B) :-
     member(First, B),
-    listSubset(Rest, B, YesOrNo).
-listSubset(_, _, YesOrNo) :- YesOrNo=no.
+    listSubset(Rest, B), !.
 
-hasInput(Func, TargetInputs, YesOrNo) :-
+hasInput(Func, TargetInputs) :-
     inputs(Func, Inputs),
-    listSubset(TargetInputs, Inputs, YesOrNo).
-hasOutput(Func, TargetOutputs, YesOrNo) :-
-    inputs(Func, Outputs),
-    listSubset(TargetOutputs, Outputs, YesOrNo).
+    listSubset(TargetInputs, Inputs).
+hasOutput(Func, TargetOutputs) :-
+    outputs(Func, Outputs),
+    listSubset(TargetOutputs, Outputs).
 
 inputConstraint(Func, Inputs, Score) :-
-    hasInput(Func, Inputs, YesOrNo),
-    YesOrNo=yes,
-    Score=1.0.
+    hasInput(Func, Inputs),
+    Score=1.0, !.
 
-inputConstraint(Func, Inputs, Score) :-
-    hasInput(Func, Inputs, YesOrNo),
-    YesOrNo=no,
-    Score=0.0.
+inputConstraint(_Func, _Inputs, Score) :- Score=0.0.
 
 % TODO: Add outputConstraint function
 % TODO: Add Levenshtein distance:
@@ -46,28 +40,27 @@ inputConstraint(Func, Inputs, Score) :-
 funcConstraints(Func, Constraints, Threshold, ScoreOut) :-
     funcConstraints(Func, Constraints, Threshold, 1.0, ScoreOut).
 
+funcConstraints(_Func, [], Threshold, ScoreIn, ScoreOut) :-
+    ScoreIn > Threshold,
+    ScoreOut = ScoreIn, !.
+
 funcConstraints(Func, [pair(ConstraintFn, Args)|Rest], Threshold, ScoreIn, ScoreOut) :-
     ScoreIn > Threshold,
     call(ConstraintFn, Func, Args, ThisScore),
     ScoreIn2 is ScoreIn * ThisScore,
-    funcConstraints(Func, Rest, Threshold, ScoreIn2, ScoreOut).
+    funcConstraints(Func, Rest, Threshold, ScoreIn2, ScoreOut), !.
 
-funcConstraints(Func, [], Threshold, ScoreIn, ScoreOut) :-
-    ScoreIn > Threshold,
-    ScoreOut = ScoreIn.
+funcConstraints(_, _, _, _, Score) :- Score = 0.
 
-funcConstraints(_, _, _, _, Score) :-
-    Score = 0.
-
-% TODO: Make this a breadth-first search
+% TODO: Make this a breadth-first A* search.
 % TODO: Add path length constraint and/or threshold to this.
 path(InputTypes, OutputTypes, []) :-
-    listSubset(InputTypes, OutputTypes, yes).
+    listSubset(InputTypes, OutputTypes).
 
 % try path(["str"], ["None"], Path). (use ; to get more than one path)
 path(InputTypes, OutputTypes, [StartFn|Rest]) :-
     inputs(StartFn, Inputs),
-    listSubset(InputTypes, Inputs, yes),
+    listSubset(InputTypes, Inputs),
     outputs(StartFn, Outputs),
     path(Outputs, OutputTypes, Rest).
 
