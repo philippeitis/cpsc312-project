594c3c37690e0a0a4a4395b86872d07e5c51284f
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-12 00:56:48 -0800

    Fix string parsing

--- prolog/main.pl
+++ prolog/main.pl
@@ -4,6 +4,7 @@
 :- use_module(string_op).
 :- use_module(search).
 :- use_module(server).
+:- use_module(library(dcg/basics)).
 
 %% pretty_print_path(List[function])
 pretty_print_path([]) :- !.
@@ -12,21 +13,42 @@ pretty_print_path([Func]) :-
 pretty_print_path([Head|Tail]) :-
     format("~w -> ", [Head]), pretty_print_path(Tail), !.
 
-%% default_docs(OutputDoc, Outputs, Docs)
-% True if Outputs is the first item of OutputDoc,
-% and Docs is either "" and OutputDoc only contains Output,
-% or Docs is the second item of Output.
-default_docs([Outputs], Outputs, "").
-default_docs([Outputs, Docs], Outputs, Docs).
+append(Left, Mid, Right, List) :-
+    append(Left, Mid, LeftMid),
+    append(LeftMid, Right, List).
 
-parse_definition(Definition, InputTypes, OutputTypes, Docs) :- 
-    split_left(Definition, " :", 1, [Inputs, OutputDoc]),
-    split_string(Inputs, ", ", ", ", InputTypes),
-    format("InputTypes is ~w", [InputTypes]), nl,
-    split_left(OutputDoc, "| ", 1, OutputDocs),
-    default_docs(OutputDocs, Outputs, Docs),
-    split_string(Outputs, ", ", ", ", OutputTypes),
-    format("OutputTypes is ~w", [OutputTypes]), nl.
+parse_sig(Name, Inputs, Outputs, Docs) -->
+    string(NameCodes),
+    {string_codes(Name, NameCodes)},
+    dcol, 
+    parse_type_sig(Inputs, Outputs),
+    optional_doc(Docs).
+
+parse_type_sig(Inputs, Outputs) -->
+    "[", list_of_types(Inputs), "]",
+    wh, "->", wh,
+    "[", list_of_types(Outputs), "]".
+
+optional_doc(Docs) --> tail, string(DocCodes), {string_codes(Docs, DocCodes)}.
+optional_doc("") --> wh.
+single_type(Type) --> string(TypeCode), {string_codes(Type, TypeCode)}.
+
+list_of_types([Type|Rem]) --> single_type(Type), wh, ",", wh, list_of_types(Rem).
+list_of_types([Type]) --> single_type(Type).
+list_of_types([]) --> [].
+
+wh -->  " ", wh.
+wh -->  "".
+dcol --> wh, "::", wh.
+tail --> wh, "|", wh.
+
+parse_signature(String, Name, Inputs, Outputs, Docs) :-
+    string_codes(String, Codes),
+    phrase(parse_sig(Name, Inputs, Outputs, Docs), Codes).
+
+parse_types(String, Inputs, Outputs) :-
+    string_codes(String, Codes),
+    phrase(parse_type_sig(Inputs, Outputs), Codes).
 
 command("define").
 command("clear").
@@ -39,16 +61,16 @@ command("quit").
 
 assist("define") :- 
     write("Defines a function from user input."), nl,
-    write("Example: define `fnName` arg1, arg2 :: output1, output2 | doc "), nl, !.
+    write("Example: define fnName :: [arg1, arg2] -> [output1, output2] | doc "), nl, !.
 assist("clear") :- 
     write("Clears the database of functions."), nl,
     write("Example: clear"), nl, !.
 assist("search") :- 
     write("Finds a function with the given signature."), nl,
-    write("Example: search arg1, arg2 :: output1, output2"), nl, !.
+    write("Example: search :: [arg1, arg2] -> [output1, output2]"), nl, !.
 assist("path") :- 
     write("Finds a sequence of function which transform the input to the output."), nl,
-    write("Example: search arg1, arg2 :: output1, output2"), nl, !.
+    write("Example: path :: [arg1, arg2] -> [output1, output2]"), nl, !.
 assist("store") :- 
     write("Persists the existing functions to disk at the provided path."), nl,
     write("Example: store ./path/to/file.json"), nl, !.
@@ -84,10 +106,9 @@ available_commands() :-
     foreach(command(Name), (format("    ~w", Name), nl)).
 
 execute_command(String) :-
-    split_left(String, " ", 2, ["define", FnName, Rest]),
-    format("fnName is ~w", [FnName]), nl,
-    write(Rest), nl,
-    parse_definition(Rest, InputTypes, OutputTypes, Docs),
+    split_left(String, " ", 1, ["define", Rest]),
+    parse_signature(Rest, FnName, InputTypes, OutputTypes, Docs),
+    format("Adding function: ~w", [FnName]), nl,
     assertz(function(FnName, InputTypes, OutputTypes, Docs)), !.
 
 execute_command("clear") :-
@@ -96,7 +117,7 @@ execute_command("clear") :-
 
 execute_command(String) :-
     split_left(String, " ", 1, ["search", Rest]),
-    parse_definition(Rest, InputTypes, OutputTypes, _Docs),
+    parse_types(Rest, InputTypes, OutputTypes),
     findnsols(5, Func, func_path_no_cycles(InputTypes, OutputTypes, [Func]), Solns),
     length(Solns, Len),
     format("Found ~w solutions:", [Len]), nl,
@@ -104,7 +125,7 @@ execute_command(String) :-
 
 execute_command(String) :-
     split_left(String, " ", 1, ["path", Rest]),
-    parse_definition(Rest, InputTypes, OutputTypes, _Docs),
+    parse_types(Rest, InputTypes, OutputTypes),
     findnsols(5, Path, func_path_no_cycles(InputTypes, OutputTypes, Path), Solns),
     length(Solns, Len),
     format("Found ~w solutions:", [Len]), nl,
