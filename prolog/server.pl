01ef87dd2fc7f1fb772df39538b84e820e95c332
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-29 14:45:09 -0800

    Add favicons to reduce errors

--- prolog/server.pl
+++ prolog/server.pl
@@ -234,7 +234,11 @@ openapi(_) :-
                     src='https://unpkg.com/rapidoc/dist/rapidoc-min.js'
                 ],
                 []
-            )
+            ),
+            \['<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">'],
+            \['<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">'],
+            \['<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">'],
+            \['<link rel="manifest" href="/site.webmanifest">']
         ],
         [
             \['<rapi-doc spec-url="./openapi.json"></rapi-doc>']
