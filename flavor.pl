0ffa1048c9ad19794486f635d2d0c4f5c47b8d94
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-28 19:08:58 -0800

    Don't add trailing slash

--- flavor.pl
+++ flavor.pl
@@ -4,5 +4,5 @@ main([Port]) :-
     ansi_format([fg(blue)], 'Started server at ', []),
     ansi_format([bold, fg(blue)], 'http://localhost:~w/~n', [Port]),
     ansi_format([fg(blue)], 'Go to ', []),
-    ansi_format([bold, fg(blue)], 'http://localhost:~w/openapi/', [Port]),
+    ansi_format([bold, fg(blue)], 'http://localhost:~w/openapi', [Port]),
     ansi_format([fg(blue)], ' for a web-based REPL~n', []).
