92a574f14823e28aba277bfe8b421a0ca0ab8302
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-30 18:06:31 -0800

    Allow returning unbound types in JSON

--- prolog/function/serde.pl
+++ prolog/function/serde.pl
@@ -12,6 +12,7 @@ jsonify_generic(generic(Name, Bounds), _{name:Name, bounds:Bounds}) :-
     is_list(Bounds), !.
 jsonify_generic(generic(Name, _), _{name:Name}).
 
+jsonify_type(unbound, _{is_unbound: true}) :- !.
 jsonify_type(Generic, JsonGeneric) :-
     jsonify_generic(Generic, JsonGeneric), !.
 jsonify_type(Type, Type) :- string(Type), !.
