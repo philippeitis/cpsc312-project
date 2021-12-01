d8a3762d0030086c6f55a67511a6cecec3421dd2
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-11-30 16:56:13 -0800

    Change flavour text

--- prolog/server.pl
+++ prolog/server.pl
@@ -57,7 +57,10 @@ server(Port) :-	\+suppress_start_msg,
     ansi_format([bold, fg(blue)], 'http://localhost:~w/~n', [Port]),
     ansi_format([fg(blue)], 'Go to ', []),
     ansi_format([bold, fg(blue)], 'http://localhost:~w/openapi', [Port]),
-    ansi_format([fg(blue)], ' for a web-based REPL~n', []), !.
+    ansi_format([fg(blue)], ' to interact with the OpenAPI specification~n', []),
+    ansi_format([fg(blue)], 'Go to ', []),
+    ansi_format([bold, fg(blue)], 'http://localhost:~w/main.html', [Port]),
+    ansi_format([fg(blue)], ' to interact with the application~n', []).
 
 shutdown(Port) :- http_stop_server(Port, []).
 
